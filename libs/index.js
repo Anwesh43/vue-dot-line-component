5560107281e307162de0c9ed931390bff75bcded
Anwesh43 <anweshthecool0@gmail.com>
2019-06-01 20:30:19 +0530

    adding Loop class

--- libs/index.js
+++ libs/index.js
@@ -1,4 +1,40 @@
 const maxHeight = window.innerHeight / 10
+
+class Loop {
+
+    constructor() {
+        this.tasks = {}
+    }
+
+    addTask(task) {
+        this.tasks.push(task)
+        if (this.tasks.length == 1) {
+
+        }
+    }
+
+    _start() {
+        this.interval = setInterval(() => {
+            this.tasks.forEach((task) => {
+                task.execute()
+            })
+        })
+    }
+
+    stop(tag) {
+        for (var i = this.tasks.length - 1; i >= 0; i--) {
+            const task  = this.tasks[i]
+            if (task.tag === tag) {
+                this.tasks.splice(i, 1)
+                break
+            }
+        }
+        if (this.tasks.length == 0) {
+            clearInterval(this.interval)
+        }
+    }
+}
+
 Vue.Component('dot-line', {
     template : '#dotlinetemplate',
     data() {
