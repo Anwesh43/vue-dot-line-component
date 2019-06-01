41aeb9c4cb5ddfacfe31558df83792906a563c5a
Anwesh43 <anweshthecool0@gmail.com>
2019-06-01 20:37:43 +0530

    adding State class

--- libs/index.js
+++ libs/index.js
@@ -54,6 +54,32 @@ class Task {
     }
 }
 
+class State {
+
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += this.dir * 0.01
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
+
 Vue.Component('dot-line', {
     template : '#dotlinetemplate',
     data() {
